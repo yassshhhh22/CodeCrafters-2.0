66f0948576c8a328144320acd90e27f0e6247674
yassshhhh22 <mryash7969@gmail.com>
2025-03-15 21:07:29 +0530

    Add Redux store setup, Redis client, and OTP verification functionality; update user routes and dependencies

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,39 +1,37 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router'
-import Login from './views/login.jsx'
-import Register from './views/register.jsx'
-import App from './App.jsx'
-import VerifyOtp from './views/verifyotp-page.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import Login from "./views/login.jsx";
+import Register from "./views/register.jsx";
+import App from "./App.jsx";
+import VerifyOtp from "./views/verifyotp-page.jsx";
 
 const router = createBrowserRouter([
   {
-    element: <App/>,
+    element: <App />,
     path: "/",
-    children:[
+    children: [
       {
-        element: <Login/>,
-        path: "/login"
+        element: <Login />,
+        path: "/login",
       },
       {
-        element: <Register/>,
-        path: "/signup"
+        element: <Register />,
+        path: "/signup",
       },
       {
-        element: <VerifyOtp/>,
-        path: "/verify-email"
+        element: <VerifyOtp />,
+        path: "/verify-email",
       },
-    ]
+    ],
+  },
+]);
 
-  }
-])
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterProvider router={router}>
-    <App />
+      <App />
     </RouterProvider>
- 
-  </StrictMode>,
-)
+  </StrictMode>
+);
